24a4615d433888bbb5acbbe2d7b57e67f0253655
Anton Zuev <anzuev@bk.ru>
2015-11-14 18:05:24 +0300

    fifth commit

--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,31 @@ var host = config.get('general:host');
 var carCounter = 0;
 var maxCarCounter = 4;
 
-var parking = {
-    firstLeft: 0,
-    firstRight: 0,
-    secondLeft: 0,
-    secondRight: 0
+var prevParkingContext = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0
 };
-var indexToPlace = ['firstLeft', "firstRight", "secondLeft", 'secondRight'];
+var changesOfParkingContext = {
+
+};
 module.exports = function(app){
 
     app.all('*', function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type');
-        console.log(parking);
         next();
 
     });
 
     app.get('/', function(req, res, next){
 
-       /* res.render('index', {
-            freeSpaces: maxCarCounter - carCounter,
-            parking: parking
-        });
-        */
+
         res.json({
             freeSpaces: maxCarCounter - carCounter,
-            parking: parking
+            parking: prevParkingContext
         });
 
         next();
@@ -40,36 +37,39 @@ module.exports = function(app){
         res.end();
         next();
     });
-    app.post('/newCar/:place', function(req, res, next){
+    app.post('/parkingContext/', function(req, res, next){
         var status = 200;
-        var place = req.params.place;
-        if((place - 1 >= 0) && (place - 1 < 4)){
-            parking[indexToPlace[place - 1]] = 1;
-            carCounter++;
-        }else{
-            status = 400;
-        }
+
+        var parkingContext ={};
+        parkingContext['1'] = req.body['5'];
+        parkingContext['2'] = req.body['6'];
+        parkingContext['3'] = req.body['7'];
+        parkingContext['4'] = req.body['8'];
+        console.log(parkingContext);
+        cmpOldParkingContextWithNew(parkingContext);
+        console.log(changesOfParkingContext);
         res.sendStatus(status);
         res.end();
-
         next();
     });
 
-    app.post('/removeCar/:place', function(req, res, next){
-        var place = req.param.place;
-        if(place - 1 >= 0 && place - 1 < 4){
-            parking[indexToPlace[place - 1]] = 0;
-            carCounter--;
+
+
+
+
+
+
+
+
+
+}
+
+function cmpOldParkingContextWithNew(newParkingContext){
+    for(var key in prevParkingContext){
+        if(newParkingContext[key] != prevParkingContext[key]){
+            changesOfParkingContext[key] = newParkingContext[key]
         }
-        res.end(200);
-        next();
-    })
-
-
-
-
-
-
-
-
+    }
+    prevParkingContext = newParkingContext;
+    return;
 }
