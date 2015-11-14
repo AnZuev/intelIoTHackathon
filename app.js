4da1e32775a52b44995f675c29bde661ba65c7f3
Anton Zuev <anzuev@bk.ru>
2015-11-14 16:26:09 +0300

    third commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var HttpError = require('./error').HttpError;
 var morgan = require('morgan');
 
 var config = require('./config');
@@ -15,19 +14,10 @@ app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-var session = require('express-session');
-app.use(session({
-    secret: config.get('session:secret'),
-    key: config.get('session:key'),
-    cookie: config.get('session:cookie'),
-    resave: false,
-    saveUninitialized: true,
-    store: require('./libs/sessionsStore')
-}));
+
 
 // development error handler
 require('./routes')(app);
